97fa0fce28aa1066a8d921d8d6d26516e5075a20
zackatack108 <zackatack108@users.noreply.github.com>
2018-01-19 17:52:47 -0700

    added comments to file

--- Capture_the_Flag/src/ctf/ArenaState.java
+++ Capture_the_Flag/src/ctf/ArenaState.java
@@ -59,81 +59,95 @@ public class ArenaState {
 
 	public void stop(String arenaName){
 
-		int arenaNum = data.getArenaNum(arenaName);
+		UUID playerID = null;
+		Player player = null;
 
-		int inGameCount = data.getArena(arenaNum).getInGameCount();
-		
-		if(inGameCount > 0){
+		//check if the arena name isn't null
+		if(arenaName != null) {
 
+			//get the arena number from the arena name
+			int arenaNum = data.getArenaNum(arenaName);
+
+			//stop the timer
 			time.stopTimer(data.getArena(arenaNum).gameTimer);
 
+			//check if red team won 
 			if(data.getArena(arenaNum).getRedScore() > data.getArena(arenaNum).getBlueScore()){
 
+				//create win message
 				data.getArena(arenaNum).setFlagMsg(ChatColor.YELLOW + "Red team wins with " + 
 						ChatColor.RED + Integer.toString(data.getArena(arenaNum).getRedScore()) + ChatColor.YELLOW + " to " +
 						ChatColor.BLUE + Integer.toString(data.getArena(arenaNum).getBlueScore()));
+
+				//display win message to players
 				playerList.getPlayer(arenaName, Message.FLAG);
 
-			} else if(data.getArena(arenaNum).getBlueScore() > data.getArena(arenaNum).getRedScore()){
+			} else if(data.getArena(arenaNum).getBlueScore() > data.getArena(arenaNum).getRedScore()){ //check if blue team won
 
+				//create win message
 				data.getArena(arenaNum).setFlagMsg(ChatColor.YELLOW + "Blue team wins with " + 
 						ChatColor.BLUE + Integer.toString(data.getArena(arenaNum).getBlueScore()) + ChatColor.YELLOW + " to " +
 						ChatColor.RED + Integer.toString(data.getArena(arenaNum).getRedScore()));
+
+				//display win message to players
 				playerList.getPlayer(arenaName, Message.FLAG);
 
-			} else if(data.getArena(arenaNum).getBlueScore() == data.getArena(arenaNum).getRedScore()){
+			} else if(data.getArena(arenaNum).getBlueScore() == data.getArena(arenaNum).getRedScore()){ //check if the teams tied
 
+				//create win message
 				data.getArena(arenaNum).setFlagMsg(ChatColor.YELLOW + "Score is tied with " + 
 						ChatColor.BLUE + Integer.toString(data.getArena(arenaNum).getBlueScore()) + ChatColor.YELLOW + " to " +
 						ChatColor.RED + Integer.toString(data.getArena(arenaNum).getRedScore()));
+
+				//display win message to players
 				playerList.getPlayer(arenaName, Message.FLAG);
 
 			}
+			for(int subscript = 0; subscript < data.getArena(arenaNum).getInGameCount(); subscript++){
 
-		}
+				//assign the playerID to the UUID of the user in the game
+				playerID = UUID.fromString(data.getArena(arenaNum).getInGame(subscript));
 
-		UUID playerID;
-		Player player = null;
+				//get the player from the UUID
+				player = Bukkit.getPlayer(playerID);
 
-		int gameCount = data.getArena(arenaNum).getInGameCount();
-		for(int subscript = 0; subscript < gameCount; subscript++){
+				//return players inventory
+				data.returnInventory(player);
 
-			inGameCount = data.getArena(arenaNum).getInGameCount();
+				//assign a new scoreboard that is empty
+				player.setScoreboard(Bukkit.getScoreboardManager().getNewScoreboard());
+
+				//teleport player to the hub
+				player.teleport(data.getHub());
+
+			}
 			
-			playerID = UUID.fromString(data.getArena(arenaNum).getInGame(subscript));
+			//stop the timer
+			time.stopTimer(data.getArena(arenaNum).gameTimer);
+			time.stopTimer(data.getArena(arenaNum).lobbyTimer);
 
-			player = Bukkit.getPlayer(playerID);
-
-			data.getArena(arenaNum).setInGameCount(--inGameCount);
-
-			data.returnInventory(player);
-
-			player.setScoreboard(Bukkit.getScoreboardManager().getNewScoreboard());
-
-			//teleport player to the hub
-			player.teleport(data.getHub());
-
-		}
-
-		inGameCount = data.getArena(arenaNum).getInGameCount();
-
-		if(inGameCount == 0){
+			//set red and blue team count to 0
 			data.getArena(arenaNum).setRedCount(0);
 			data.getArena(arenaNum).setBlueCount(0);
 
+			//set red and blue scores to 0
 			data.getArena(arenaNum).setRedScore(0);
 			data.getArena(arenaNum).setBlueScore(0);
 
+			//clear the teams and arena of players
 			data.getArena(arenaNum).setInGame(null);
 			data.getArena(arenaNum).setRedTeam(null);
 			data.getArena(arenaNum).setBlueTeam(null);
 
+			//set who has what flag to empty
 			data.getArena(arenaNum).setBlueHas(null);
 			data.getArena(arenaNum).setRedHas(null);
 
+			//set the flags taken to false
 			data.getArena(arenaNum).setRedTaken(false);
 			data.getArena(arenaNum).setBlueTaken(false);
 
+			//change the arena state and save the config
 			main.getConfig().set("Arenas." + arenaName + ".State", "waiting for players");
 			main.saveConfig();
 		}
@@ -151,7 +165,7 @@ public class ArenaState {
 
 			playerID = UUID.fromString(data.getArena(arenaNum).getInGame(count));
 			player = Bukkit.getPlayer(playerID);
-			
+
 			if(count%2 == 0){
 				player.teleport(data.getRedSpawn(arenaName));
 				data.getArena(arenaNum).setRedTeam(player);
